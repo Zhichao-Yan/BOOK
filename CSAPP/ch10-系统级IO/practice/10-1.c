4c109c712b132616303ba3fec84eb70e42c35ef2
yanzhichao <1594047159@qq.com>
2024-05-16 23:26:30 +0800

    10-1

--- CSAPP/ch10-系统级IO/practice/10-1.c
+++ CSAPP/ch10-系统级IO/practice/10-1.c
@@ -3,9 +3,10 @@
 int main()
 {
     int fd1, fd2;
-    fd1 = Open("foo.txt",O_RDONLY,0);
+    fd1 = Open("foo.txt",O_RDONLY|O_CREAT,0);
     Close(fd1);
-    fd2 = Open("baz.txt",O_RDONLY,0);
+    fd2 = Open("baz.txt",O_RDONLY|O_CREAT,0);
     printf("fd2 = %d\n", fd2);
     exit(0);
 }
+// print->fd2 = 3;
